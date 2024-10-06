ffd20ebf7801246d09964d122d6d61cccee9ce4c
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2024-10-05 22:57:51 -0300

    wine proton
    teste apenas wine proton

--- packages/62-wine/build.sh
+++ packages/62-wine/build.sh
@@ -1,4 +1,4 @@
-GIT_URL=https://github.com/wine-mirror/wine
+GIT_URL=https://github.com/ValveSoftware/Proton
  BLACKLIST_ARCHITECTURE=aarch64 
  HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x" 
  HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools" 
