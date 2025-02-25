f2b027528be81f6391db9e3b5ac707f989809c60
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-02-24 21:56:31 -0300

    musa-vulkan-freedreno
    git commit

--- packages/mesa-vulkan-freedreno/build.sh
+++ packages/mesa-vulkan-freedreno/build.sh
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCHITECTURE=x86_64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=365cd04375baeb8c607c1a49e85e9a364e193a08
+GIT_COMMIT=3c15dba46d6ed64916cb0fcb6d82e82e5b254173
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
