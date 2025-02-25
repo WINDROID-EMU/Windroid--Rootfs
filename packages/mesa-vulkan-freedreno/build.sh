f97bce3e21ce2ae2310f883f2ddd81a9b7a6f098
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-02-24 22:28:47 -0300

    Driver mesa 23.3.6

--- packages/mesa-vulkan-freedreno/build.sh
+++ packages/mesa-vulkan-freedreno/build.sh
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCHITECTURE=x86_64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=3c15dba46d6ed64916cb0fcb6d82e82e5b254173
+GIT_COMMIT=d46e76ba84c36d35d1ec8267ae0d8cb68088cf27
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
