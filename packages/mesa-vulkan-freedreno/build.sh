7070f21f8a39618a46dab8d5d693ee572ef7cd10
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-02-28 16:38:49 -0300

    MESA 21.1.7 REGRESSÃO TESTE

--- packages/mesa-vulkan-freedreno/build.sh
+++ packages/mesa-vulkan-freedreno/build.sh
@@ -1,4 +1,4 @@
-PKG_VER=25.0.0-devel-[gss]
+PKG_VER=21.1.7-devel-[gss]
 PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Turnip Driver"
 VK_DRIVER_LIB="libvulkan_freedreno.so"
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCHITECTURE=x86_64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=d46e76ba84c36d35d1ec8267ae0d8cb68088cf27
+GIT_COMMIT=5edae4604e6af456cb0a0e2aa3c2fb79f302971f
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
